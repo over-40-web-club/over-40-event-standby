9f84e8943e23df2f380f124339b3e3c2693dac77
pitang1965 <pitang1965@gmail.com>
2024-12-01 14:01:16 +0900

    メッセージを更新

--- src/components/EventInfo.tsx
+++ src/components/EventInfo.tsx
@@ -4,7 +4,8 @@ export const EventInfo = () => {
       <h1 className="text-3xl font-bold mb-4">
         40代から IT 業界への転身した６人の話を聞く会
       </h1>
-      <p className="text-xl text-blue-200">定刻どおり開始いたします</p>
+      <p className="text-xl text-blue-200">本日はご参加誠にありがとうございます。</p>
+      <p className="text-xl text-blue-200">定刻どおりに開始いたします。</p>
     </div>
   );
 };
--- src/components/ParticipationGuidelines.tsx
+++ src/components/ParticipationGuidelines.tsx
@@ -5,15 +5,15 @@ export const ParticipationGuidelines = () => {
       <ul className="text-left space-y-3 max-w-xl mx-auto">
         <li className="flex items-start space-x-2">
           <span className="text-blue-300">•</span>
-          <span>マイクOFFをお願いいたします</span>
+          <span>マイクOFFをお願いいたします。</span>
         </li>
         <li className="flex items-start space-x-2">
           <span className="text-blue-300">•</span>
-          <span>質問等はチャット機能をご利用ください</span>
+          <span>質問等はチャット機能をご利用ください。宛先は「全員」を指定してください。</span>
         </li>
         <li className="flex items-start space-x-2">
           <span className="text-blue-300">•</span>
-          <span>チャットの宛先は「全員」を指定してください</span>
+          <span>公開はしませんが、録画をおこないます。</span>
         </li>
       </ul>
     </div>
